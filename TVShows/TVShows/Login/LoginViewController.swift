4e61d036266d272c75fd707ff7228946120dbb09
Filip Ostojic <ficoostojic.ostojic@gmail.com>
2019-07-15 16:06:40 +0200

    add login screen

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -11,82 +11,35 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
-    private var counter = 0;
-    @IBOutlet private weak var counterLabel: UILabel!
-    @IBOutlet private weak var clickButton: UIButton!
+    // MARK: outlets
+    
+    @IBOutlet private weak var checkBtn: UIButton!
+    @IBOutlet private weak var loginBtn: UIButton!
+    @IBOutlet private weak var registerBtn: UIButton!
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    
+    // MARK: properties
+    
+    private var checked = false
+    
+    // MARK: lifecycle functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initLabel()
-        initButton()
     }
     
-    private func initLabel() -> Void {
-        counterLabel.text = String(counter)
-    }
+    // MARK: actions
     
-    private func initButton() -> Void {
-        clickButton.layer.borderColor = #colorLiteral(red: 0.476841867, green: 0.5048075914, blue: 1, alpha: 1)
-        clickButton.layer.borderWidth = 2
-        clickButton.layer.cornerRadius = 15
-    }
-    
-    @IBAction private func buttonClicked(_ sender:UIButton){
-        counter += 1
-        counterLabel.text = String(counter)
-        shake()
-        changeColor()
-    }
-    
-    private func changeColor() -> Void {
-        let redFloat = CGFloat.random(in: 0 ... 1)
-        let greenFloat = CGFloat.random(in: 0 ... 1)
-        let blueFloat = CGFloat.random(in: 0 ... 1)
+    @IBAction func checkChange(_ sender: UIButton) {
         
-        counterLabel.backgroundColor = UIColor(
-            red: redFloat,
-            green: greenFloat,
-            blue: blueFloat,
-            alpha: 1.0
-        )
-        
-        if (counterLabel.backgroundColor?.isLight())! {
-            counterLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        if !checked {
+            sender.setImage(UIImage(named: "ic-checkbox-filled"), for: .normal)
         } else {
-            counterLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            sender.setImage(UIImage(named: "ic-checkbox-empty"), for: .normal)
         }
-    }
-    
-    private func shake() {
-        let shake = CABasicAnimation(keyPath: "position")
-        shake.duration = 0.1
-        shake.repeatCount = 2
-        shake.autoreverses = true
         
-        let fromPoint = CGPoint(x: clickButton.center.x - 5, y: clickButton.center.y)
-        let fromValue = NSValue(cgPoint: fromPoint)
-        
-        let toPoint = CGPoint(x: clickButton.center.x + 5, y: clickButton.center.y)
-        let toValue = NSValue(cgPoint: toPoint)
-        
-        shake.fromValue = fromValue
-        shake.toValue = toValue
-        
-        clickButton.layer.add(shake, forKey: "position")
+        checked = checked ? false : true;
     }
     
 }
-
-extension UIColor {
-    func isLight() -> Bool {
-        guard
-            let components = cgColor.components
-            else {
-                return false
-        }
-        
-        let brightness = ((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000
-        return brightness > 0.5
-    }
-}
-
