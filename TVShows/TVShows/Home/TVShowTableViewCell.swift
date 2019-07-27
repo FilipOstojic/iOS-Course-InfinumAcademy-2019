db6b581b044f56584a3f93070104e22d7c4e0b62
Filip Ostojic <ficoostojic.ostojic@gmail.com>
2019-07-27 18:11:35 +0200

    add pod Kingfisher

--- TVShows/TVShows/Home/TVShowTableViewCell.swift
+++ TVShows/TVShows/Home/TVShowTableViewCell.swift
@@ -13,6 +13,7 @@ class TVShowTableViewCell: UITableViewCell {
     // MARK: - Outlets
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var thumbnail: UIImageView!
     
     // MARK: - LifeCycle methods
     
@@ -27,7 +28,7 @@ class TVShowTableViewCell: UITableViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         titleLabel.text = nil
-        //resetirat ostalo kasnije
+        thumbnail.image = UIImage(named: "login-logo")
     }
 
 }
@@ -36,7 +37,8 @@ extension TVShowTableViewCell {
     
     func configure(with item: Show) {
         titleLabel.text = item.title
-        //ostalo pridružit kasnije
+//        let url = URL(string: "https://api.infinum.academy" + item.imageUrl)
+//        thumbnail.kf.setImage(with: url)
     }
     
 }
